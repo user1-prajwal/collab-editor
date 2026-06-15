984f7798eca414334c70299dd8931bbeb93840d1
user1-prajwal <ewitewit451@gmail.com>
2026-06-15 12:43:29 +0530

    fix: seperate constants file from app file

---
+++ collab-editor-frontend/src/constants.js
@@ -0,0 +1,5 @@
+export const LANGUAGES = ['javascript', 'python', 'cpp', 'java', 'typescript']
+export const COLORS = ['#FF6B6B', '#4ECDC4', '#42F527', '#ffffff', '#fc15fc']
+export const SESSION_COLOR = COLORS[Math.floor(Math.random() * COLORS.length)]
+export const BACKEND_URL = 'https://collab-editor-backend-p7at.onrender.com'
+// export const BACKEND_URL = 'http://localhost:4000'
